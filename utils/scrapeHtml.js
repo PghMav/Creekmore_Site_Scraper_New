b73723c72c852483bb72f5e7b1cca4872a8566f8
Bret Mavrich <bretmavrich@gmail.com>
2020-05-19 10:01:03 -0400

    urlFilter in place for use

--- utils/scrapeHtml.js
+++ utils/scrapeHtml.js
@@ -23,6 +23,46 @@ const scrapeHtml =  (urlArray, dir, blog) =>{
        pathname
      } = theUrl.parse(url);
 
+     //pathname filter here
+     const urlFilter = [
+       '/hunter-douglas-window-treatments/duette',
+       '/hunter-douglas/sheers-shadings',
+       '/hunter-douglas/roman-shades',
+       '/hunter-douglas/roller-shades-solar-shades',
+       '/hunter-douglas/woven-woods',
+       '/hunter-douglas/shutters',
+       '/hunter-douglas/vertical-blinds',
+       '/hunter-douglas/wood-metal-blinds',
+       '/hunter-douglas-window-treatments/design-studio-side-panels-drapery',
+       '/hunter-douglas-powerview-motorization',
+       '/new-hunter-douglas-window-treatments-and-options',
+       '/hunter-douglas-videos',
+       '/decorating-with-green-home-accents',
+       '/decorating-with-bookshelves',
+       'https://www.allaboutblindsetc.com/upgrade-your-home-office',
+       '/fresh-design-ideas-for-your-home',
+       '/window-treatments-for-bathrooms',
+       '/window-treatments-for-bedrooms',
+       '/window-treatments-for-home-offices',
+       '/window-treatments-for-kitchens',
+       '/window-treatments-for-living-rooms',
+       '/window-treatments-for-media-rooms',
+       '/window-treatments-for-nursery-and-childrens-rooms',
+       '/window-treatments-for-angled-windows',
+       '/window-treatments-for-arched-windows',
+       '/window-treatments-for-bay-and-corner-windows',
+       '/window-treatments-for-french-doors',
+       '/sidelight-window-blinds-and-shades',
+       '/window-treatments-for-skylights',
+       '/window-treatments-for-patio-doors-and-sliding-glass-doors',
+       '/motorized-blinds-shades-and-honeycombs',
+       '/room-darkening-and-blackout-window-treatments',
+       '/child-and-pet-safe-window-treatments',
+       '/energy-efficient-window-coverings',
+       '/light-filtering-window-treatments-and-uv-protection'
+
+     ]
+
      const pathnameLength = pathname.length<2
 
      const ifBlog = blog ? `/blog`:``
@@ -36,6 +76,20 @@ const scrapeHtml =  (urlArray, dir, blog) =>{
     .then(html => {
 
       fs.writeFileSync(htmlPath, html)
+      return html
+    })
+    .then(html=>{
+
+      //STAGE: CSS SCRAPE AND FILE
+      //Scrape for the link tag using cheerio; save as variable
+      // check this url to see if it is the home page via pathnameLength
+      //IF YES: run the tag link through getCss(), then change the link
+      //using changeCssPath()
+      //IF NO: just change the css link using changeCssPath
+
+      return html
+       // getCss(html)
+       // changeCSsPath()
 
     })
     .catch(e=>{
@@ -59,14 +113,14 @@ process.on('exit', ()=>{
 
   });
 }
-// const directory = 'www.mrksquincy.com'
 
 
+const directory = 'www.mrksquincy.com'
 
-// basicUrls = [
-//   'https://www.mrksquincy.com',
-//   'https://www.mrksquincy.com/about',
-//   'https://www.mrksquincy.com/blog']
-// scrapeHtml(basicUrls, directory)
+basicUrls = [
+  'https://www.mrksquincy.com',
+  'https://www.mrksquincy.com/about',
+  'https://www.mrksquincy.com/blog']
+scrapeHtml(basicUrls, directory)
 
-module.exports = scrapeHtml
+// module.exports = scrapeHtml
